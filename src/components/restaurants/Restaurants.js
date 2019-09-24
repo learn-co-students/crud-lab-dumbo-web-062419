ead787c80c986f821702ba5b2644d9ca7bc330d4
github username <github email address>
2019-09-24 14:28:33 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
   render() {
+    const rants = this.props.restaurants.map(restaurant => {
+      return <li><Restaurant restaurant={restaurant} /></li>
+    })
     return(
       <ul>
-        Restaurants Component
+        {rants}
       </ul>
     );
   }
